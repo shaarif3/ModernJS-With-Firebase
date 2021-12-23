e509d48adace250ffffa9ca24d3a044a05258922
Shaarif <shaarifshahid@gmail.com>
2021-12-23 20:29:58 +0500

    Concat Method

---
+++ concat.js
@@ -0,0 +1,16 @@
+const firstName = "shaarif";
+const lastName = "shahid";
+let val;
+
+//concat
+val = firstName + " " +lastName
+//append
+val = firstName;
+val+=lastName;
+//escaping
+val= " hi my name is shaarif , and its Alpha's phone"
+//concat
+val =firstName.concat(' '+ lastName)
+//uppercase
+val= firstName.toUpperCase() + " " + lastName.toUpperCase()
+console.log(val)
