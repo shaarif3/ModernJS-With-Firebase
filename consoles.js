1aff4a9ecb27ea56335b479c96594231dca3a35b
Shaarif <shaarifshahid@gmail.com>
2021-12-23 13:28:24 +0500

    consoles

--- consoles.js
+++ consoles.js
@@ -2,3 +2,21 @@ console.log("Hello ModernJs-Es6");
 console.log(223190, "console Integer");
 var Array = [12,22,31,09,81];
 console.log(Array,"Console Array");
+var object = [
+    {
+    name: "shaarif",
+    age: "22",
+},
+    {
+    name: "Alpha",
+    age: "10",
+},
+    {
+    name: "beta",
+    age: "16",
+},
+]
+console.table(object);
+console.error("Error 404!");
+console.warn("Warning Enter Correct Fields!");
+// console.clear() //clear all the consoles
