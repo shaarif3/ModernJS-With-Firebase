cf510b0bf00ef67712b7f7ccbde29bc1d2d40433
Shaarif <shaarifshahid@gmail.com>
2021-12-23 15:20:36 +0500

    Math-Functions

---
+++ math_model.js
@@ -0,0 +1,33 @@
+const num1 = 8;
+const num2 = 2;
+//add
+val= num1+num2;
+//sub
+val= num1-num2;
+// multiply
+val= num1*num2;
+//remainder
+val= num1%num2;
+// power
+val = Math.pow(num1,num2);
+// roundoff
+val = Math.round(10.332)
+// square-root
+val = Math.sqrt(64)
+//remove neg sign
+val = Math.abs(-14)
+//round off to ground
+val = Math.floor(10.56)
+//round off to top
+val = Math.ceil(10.56)
+//find Min
+val = Math.min(895,231,55)
+//find max
+val = Math.max(895,231,55)
+// generate random no btw 0 & 1
+val = Math.random()
+// generate random no btw 1 & 5
+val = Math.random()*5
+// generate random no btw 1 & 5 and round off
+val = Math.round(Math.random()*5+1)
+console.log(val)
--- prim-ref.js
+++
@@ -1,8 +0,0 @@
-let user = {name:"shaarif"};
-function callme(user){
-    user.name="Alpha";
-    console.log("name inside fn", user.name);
-
-}
-callme(user);
-console.log(user.name);
