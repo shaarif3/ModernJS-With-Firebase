b718bd6f299a7f1eab00b066acb90cfb5a18e198
Shaarif <shaarifshahid@gmail.com>
2021-12-23 14:27:39 +0500

    variables

--- variable.js
+++ variable.js
@@ -1,22 +1,8 @@
-console.log("Hello ModernJs-Es6");
-console.log(223190, "console Integer");
-var Array = [12,22,31,09,81];
-console.log(Array,"Console Array");
-var object = [
-    {
-    name: "shaarif",
-    age: "22",
-},
-    {
-    name: "Alpha",
-    age: "10",
-},
-    {
-    name: "beta",
-    age: "16",
-},
-]
-console.table(object);
-console.error("Error 404!");
-console.warn("Warning Enter Correct Fields!");
-// console.clear() //clear all the consoles
+var UserName = "shaarif";
+console.log(UserName)
+var UserName = "Alpha"
+console.log(UserName)
+var message;
+console.log(message)
+var message = "hi my name is shaarif and i am practising es6";
+console.log(message)
