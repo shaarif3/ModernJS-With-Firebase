29a7f1ad1649da06bfc1cb48b02e0676c86d08bf
Shaarif <shaarifshahid@gmail.com>
2021-12-23 14:40:25 +0500

    Let-Const

---
+++ let_const.js
@@ -0,0 +1,19 @@
+let myName = "shaarif";
+console.log(myName);
+ myName = "alpha";
+console.log(myName);
+// it overwirtes
+
+const myName = "Shaarif";
+console.log(myName);
+myName = "alpha"
+console.log(myName)
+// it wont overwrite
+
+
+const myName = {name:"Shaarif",age:22,phone_no: +923315685256}
+console.log(myName);
+myName.name = "Alpha"
+console.log(myName);
+//Scope only inside {}
+
--- variable.js
+++
@@ -1,8 +0,0 @@
-var UserName = "shaarif";
-console.log(UserName)
-var UserName = "Alpha"
-console.log(UserName)
-var message;
-console.log(message)
-var message = "hi my name is shaarif and i am practising es6";
-console.log(message)
