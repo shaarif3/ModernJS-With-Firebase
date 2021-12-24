7a7424467c8cdbd600bc043ea753735467b051be
Shaarif <shaarifshahid@gmail.com>
2021-12-24 15:19:37 +0500

    Template-Literals

---
+++ temp-literal.jsx
@@ -0,0 +1,14 @@
+let Name = "shaarif ";
+let age = 20;
+function drink(){
+    if(age>30){
+      return "Coca-Cola"
+    }
+    else{
+        return "Fanta"
+    }
+}
+let message = `My name is ${Name}.I love drinking ${drink()}.My age is ${age} `;
+console.log(message);
+
+//Template Literals
