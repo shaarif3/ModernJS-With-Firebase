f7356707c5505c8b114c7b9b79c8c83e7902eaf5
Shaarif <shaarifshahid@gmail.com>
2021-12-24 15:36:18 +0500

    Template-Literals

--- temp-literal.jsx
+++ temp-literal.jsx
@@ -1,14 +1,14 @@
 let Name = "shaarif ";
-let age = 20;
-function drink(){
-    if(age>30){
-      return "Coca-Cola"
-    }
-    else{
-        return "Fanta"
-    }
-}
-let message = `My name is ${Name}.I love drinking ${drink()}.My age is ${age} `;
+let age = 10;
+// function drink(){
+//     if(age>=30){
+//       return "Coca-Cola"
+//     }
+//     else{
+//         return "Fanta"
+//     }
+// }
+let message = `My name is ${Name}.I love drinking ${age>=20?"Coca-Cola":"Fanta"}.My age is ${age} `;
 console.log(message);
 
 //Template Literals
