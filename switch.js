9edb0af7f92f72bad5ef1d3df415cc7fbaa2ce2d
Shaarif <shaarifshahid@gmail.com>
2021-12-25 17:02:00 +0500

    Switch Cases

---
+++ switch.js
@@ -0,0 +1,20 @@
+let fruit = "apple";
+switch (fruit) {
+    case "apple":
+        console.log("its Apple")
+        
+        break;
+    case "banana":
+        console.log("its banana")
+        
+        break;
+    case "strawberry":
+        console.log("its strawberry")
+        
+        break;
+    
+
+    default:
+        console.log("not found")
+        break;
+}
